92990a8f19baa918de6a0b53f4f9d631a13b1905
gamma-delta <petrathekat@gmail.com>
2022-04-08 00:17:38 -0500

    [Release] Make headpatting less janky

--- src/main/java/at/petrak/paucal/common/misc/PatPat.java
+++ src/main/java/at/petrak/paucal/common/misc/PatPat.java
@@ -7,6 +7,7 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -26,13 +27,15 @@ public class PatPat {
         }
 
         var player = evt.getPlayer();
-        if (player.getItemInHand(evt.getHand()).isEmpty()
-            && player.isDiscrete() && evt.getTarget() instanceof Player target) {
+        var hand = evt.getHand();
+        if (player.getItemInHand(hand).isEmpty()
+            && player.isDiscrete() && hand == InteractionHand.MAIN_HAND
+            && evt.getTarget() instanceof Player target) {
             if (player.getLevel() instanceof ServerLevel world) {
                 var pos = target.getEyePosition();
                 world.sendParticles(ParticleTypes.HEART, pos.x, pos.y + 0.5, pos.z, 1, 0, 0, 0, 0.1);
             } else {
-                player.swing(evt.getHand());
+                player.swing(hand);
             }
 
             var contributor = Contributors.getContributor(target.getUUID());
@@ -44,8 +47,9 @@ public class PatPat {
                     if (sound != null) {
                         var pitchCenter = Objects.requireNonNullElse(contributor.getFloat("paucal:patPitchCenter"), 1f);
                         target.getLevel()
-                            .playSound(target, target, sound, SoundSource.PLAYERS, 1f,
-                                pitchCenter + (float) (Math.random() - 0.5) * 0.5f);
+                            .playSound(player,
+                                target.getX(), target.getEyeY(), target.getZ(),
+                                sound, SoundSource.PLAYERS, 1f, pitchCenter + (float) (Math.random() - 0.5) * 0.5f);
                     }
                 }
             }
