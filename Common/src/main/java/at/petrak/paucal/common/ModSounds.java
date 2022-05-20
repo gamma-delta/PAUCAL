1caa190d9a28cab375619d6ed01e226aade68010
gamma-delta <petrathekat@gmail.com>
2022-05-20 15:04:14 -0500

    [Release] 0.4.0 Release!
    Not much happening from a content POV but it's now on Fabric! ... which
    will be helpful once I release Hexcasting heh

--- Common/src/main/java/at/petrak/paucal/common/ModSounds.java
+++ Common/src/main/java/at/petrak/paucal/common/ModSounds.java
@@ -11,7 +11,11 @@ import java.util.function.BiConsumer;
 public class ModSounds {
     private static final List<SoundEvent> SOUNDS = new ArrayList<>();
 
-    public static SoundEvent SPLAT = sound("splat");
+    public static SoundEvent BEEP = sound("pet.device");
+    public static SoundEvent SPLAT = sound("pet.splat");
+    public static SoundEvent NEKO = sound("pet.neko");
+    public static SoundEvent SLIME = sound("pet.slime");
+    public static SoundEvent WIRE = sound("pet.wire");
 
     private static SoundEvent sound(String name) {
         var sound = new SoundEvent(new ResourceLocation(PaucalAPI.MOD_ID, name));
