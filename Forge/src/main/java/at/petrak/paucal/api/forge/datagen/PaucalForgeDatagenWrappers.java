bef7847cdbb7608a54b95230022292ad9303b9e1
gamma-delta <petrathekat@gmail.com>
2022-05-26 08:34:09 -0500

    more wrappers

--- Forge/src/main/java/at/petrak/paucal/api/forge/datagen/PaucalForgeDatagenWrappers.java
+++ Forge/src/main/java/at/petrak/paucal/api/forge/datagen/PaucalForgeDatagenWrappers.java
@@ -1,7 +1,9 @@
 package at.petrak.paucal.api.forge.datagen;
 
 import at.petrak.paucal.forge.mixin.ForgeAccessorAdvancementProvider;
+import at.petrak.paucal.forge.mixin.ForgeAccessorTagsProvider;
 import net.minecraft.data.advancements.AdvancementProvider;
+import net.minecraft.data.tags.TagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 /**
@@ -14,5 +16,10 @@ public final class PaucalForgeDatagenWrappers {
     public static AdvancementProvider addEFHToAdvancements(AdvancementProvider advs, ExistingFileHelper efh) {
         ((ForgeAccessorAdvancementProvider) advs).paucal$setFileHelper(efh);
         return advs;
+    }
+
+    public static <T> TagsProvider<T> addEFHToTagProvider(TagsProvider<T> tags, ExistingFileHelper efh) {
+        ((ForgeAccessorTagsProvider) tags).paucal$setExistingFileHelper(efh);
+        return tags;
     }
 }
