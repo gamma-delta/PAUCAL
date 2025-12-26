90db95de55619e5b4ac7aa413127c677b82a8cba
Noelle <elijahs516@gmail.com>
2025-12-25 19:14:12 -0600

    * Fixed packet registration

--- Common/src/main/java/at/petrak/paucal/xplat/PaucalMod.java
+++ Common/src/main/java/at/petrak/paucal/xplat/PaucalMod.java
@@ -5,9 +5,18 @@ import at.petrak.paucal.xplat.common.ContributorsManifest;
 import at.petrak.paucal.xplat.common.ModRegistries;
 import at.petrak.paucal.xplat.common.msg.MsgHeadpatSoundS2C;
 import at.petrak.paucal.xplat.common.msg.MsgReloadContributorsS2C;
+import dev.architectury.impl.NetworkAggregator;
 import dev.architectury.networking.NetworkManager;
+import dev.architectury.platform.Platform;
+import dev.architectury.utils.Env;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.List;
 
 public class PaucalMod {
   public static final Logger LOGGER = LoggerFactory.getLogger(PaucalAPI.MOD_ID);
@@ -20,9 +29,15 @@ public class PaucalMod {
 
     ContributorsManifest.loadContributors();
 
-    NetworkManager.registerReceiver(NetworkManager.s2c(), MsgHeadpatSoundS2C.TYPE, MsgHeadpatSoundS2C.CODEC,
-        (pkt, buf) -> MsgHeadpatSoundS2C.handle(pkt));
-    NetworkManager.registerReceiver(NetworkManager.s2c(), MsgReloadContributorsS2C.TYPE, MsgReloadContributorsS2C.CODEC,
-        (pkt, buf) -> MsgReloadContributorsS2C.handle(pkt));
+      registerS2C(MsgHeadpatSoundS2C.TYPE, MsgHeadpatSoundS2C.CODEC, (pkt, buf) -> MsgHeadpatSoundS2C.handle(pkt));
+      registerS2C(MsgReloadContributorsS2C.TYPE, MsgReloadContributorsS2C.CODEC, (pkt, buf) -> MsgReloadContributorsS2C.handle(pkt));
   }
+    // https://github.com/Buuz135/FindMe/commit/b73519f2d9918e3dfb9b2655178230270dcdd7e0
+    private static <T extends CustomPacketPayload> void registerS2C(CustomPacketPayload.Type<T> packetType, StreamCodec<? super RegistryFriendlyByteBuf, T> codec, NetworkManager.NetworkReceiver<T> receiver) {
+        if (Platform.getEnvironment().equals(Env.SERVER)) {
+            NetworkAggregator.registerS2CType(packetType, codec, List.of());
+        } else {
+            NetworkAggregator.registerReceiver(NetworkManager.s2c(), packetType, codec, Collections.emptyList(), receiver);
+        }
+    }
 }
